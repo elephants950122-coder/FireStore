cea8dab2d0a93e2bd1169727827039beec7fe68d
lin <elephants950122@gmail.com>
2025-11-24 16:05:22 +0800

    v.3_新增/修改/刪除資料(設定文件ID)

--- app/src/main/java/tw/edu/pu/csim/tcyang/firestore/UserScoreScreen.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/firestore/UserScoreScreen.kt
@@ -20,11 +20,28 @@ fun UserScoreScreen( userScoreViewModel: UserScoreViewModel = viewModel()
     ) {
         Button(onClick = {
             // 在按鈕點擊時，直接呼叫 ViewModel 的函式
-            var userScore = UserScoreModel("子青", 39)
+            var userScore = UserScoreModel("建宇", 39)
             userScoreViewModel.addUser(userScore)
         }) {
             Text("新增資料")
         }
+
+        Button(onClick = {
+            // 在按鈕點擊時，直接呼叫 ViewModel 的函式
+            var userScore = UserScoreModel("建宇", 21)
+            userScoreViewModel.updateUser(userScore)
+        }) {
+            Text("新增/異動資料")
+        }
+
+        Button(onClick = {
+            // 在按鈕點擊時，直接呼叫 ViewModel 的函式
+            var userScore = UserScoreModel("建宇", 21)
+            userScoreViewModel.deleteUser(userScore)
+        }) {
+            Text("刪除資料")
+        }
+
         Text(userScoreViewModel.message)
     }
 }
